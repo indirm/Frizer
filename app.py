091985446fb99b59924c0be56075b44c82021b1b
indirm <indirmuji@gmail.com>
2026-02-12 16:37:56 +0000

    tweak

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ def book():
 
     return jsonify({"message": "Booked!"})
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
+
